7fbc87c146ba86ceb65d681bfbb47b222a5c8a93
RinZ <supawit.promma6256@gmail.com>
2025-07-06 03:51:43 +0700

    Add error lens

---
+++ lua/rin/lazy/error-lens.lua
@@ -0,0 +1,10 @@
+-- https://github.com/chikko80/error-lens.nvim
+
+return {
+  "chikko80/error-lens.nvim",
+  event = "BufRead",
+  dependencies = {
+    "nvim-telescope/telescope.nvim"
+  },
+  opts = {},
+}
