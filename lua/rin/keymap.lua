f36890c67af4cbc6a251bf022673fa5b15b8ee41
RinZ <supawit.promma6256@gmail.com>
2025-07-06 02:25:40 +0700

    Add LSP

--- lua/rin/keymap.lua
+++ lua/rin/keymap.lua
@@ -1,14 +1,43 @@
 local which_key = require("which-key")
 
+-- yoink from https://github.com/VVoruganti/dotfiles/blob/master/neovim/lua/marshmalon/remap.lua
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('user_lsp_attach', { clear = true }),
+  callback = function(event)
+    local mappings = {
+      { "<leader>l",  buffer = 1,                   group = "LSP" },
+      { "<leader>la", vim.lsp.buf.code_action,      buffer = 1,   desc = "Code action" },
+      { "<leader>ld", vim.diagnostic.open_float,    buffer = 1,   desc = "Open diagnostic float" },
+      { "<leader>ln", vim.lsp.buf.rename,           buffer = 1,   desc = "Rename" },
+      { "<leader>lr", vim.lsp.buf.references,       buffer = 1,   desc = "References" },
+      { "<leader>lw", vim.lsp.buf.workspace_symbol, buffer = 1,   desc = "Workspace symbol" },
+      { "K",          vim.lsp.buf.hover,            buffer = 1,   desc = "Show hover information" },
+      { "gd",         vim.lsp.buf.definition,       buffer = 1,   desc = "Go to definition" },
+      { "gl",         vim.diagnostic.open_float,    buffer = 1,   desc = "Open diagnostic float" },
+    }
+
+    which_key.add(mappings)
+
+    -- https://www.mitchellhanberg.com/modern-format-on-save-in-neovim/
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = event.buf,
+      callback = function()
+        vim.lsp.buf.format { async = false, id = event.data.client_id }
+      end
+
+    })
+  end,
+})
+
 local non_lsp_mappings = {
-  { "<leader>e", vim.cmd.Ex, desc = "Open file explorer" },
+  { "<leader>e", vim.cmd.Ex,                                             desc = "Open file explorer" },
   { "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], desc = "Search and replace word under cursor" },
 }
 
 local telescope_mappings = {
-  { "<leader>f", group = "Find" },
+  { "<leader>f",  group = "Find" },
   { "<leader>ff", "<CMD>Telescope find_files<CR>", desc = "Find file" },
-  { "<leader>fl", "<CMD>Telescope live_grep<CR>", desc = "Live grep" },
+  { "<leader>fl", "<CMD>Telescope live_grep<CR>",  desc = "Live grep" },
 }
 
 which_key.add(non_lsp_mappings)
