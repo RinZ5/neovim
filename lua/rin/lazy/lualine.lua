fb0ce42c2512f42459215b8cf4b183c03d50e0b4
RinZ <supawit.promma6256@gmail.com>
2025-07-06 20:10:18 +0700

    Remove fileformat from lualine

--- lua/rin/lazy/lualine.lua
+++ lua/rin/lazy/lualine.lua
@@ -4,6 +4,15 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require("lualine").setup({})
+    require("lualine").setup({
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+    })
   end
 }
