cb6fac197052de116687afcf81a4ab42aa206d2b
RinZ <supawit.promma6256@gmail.com>
2025-07-08 01:04:02 +0700

    Add keybind to toggle Obsidian nvim UI

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -10,15 +10,29 @@ vim.opt_local.expandtab = true
 vim.opt_local.smartindent = true
 
 local which_key = require("which-key")
+local conceallevel = 1
 
 local obsidian_mappings = {
-  { "<leader>o",  group = "Obsidian" },
+  { "<leader>o",  group = "[Obsidian]" },
   { "<leader>on", "<CMD>ObsidianTemplate note<CR>", desc = "Insert note template" },
-  { "<leader>ot", "<CMD>ObsidianTemplate<CR>",      desc = "Select template" },
+  { "<leader>oo", "<CMD>ObsidianTemplate<CR>",      desc = "Select template" },
   {
     "<leader>of",
     "<CMD>:s/\\(-\\|\\)\\(\\w\\)\\(\\w*\\)/\\=(submatch(1) == '-' ? ' ' : '') . toupper(submatch(2)) . tolower(submatch(3))/g | noh<CR>",
     desc = "Format title"
+  },
+  {
+    "<leader>oi",
+    function()
+      if conceallevel == 1 then
+        vim.cmd("set conceallevel=0")
+        conceallevel = 0
+      else
+        vim.cmd("set conceallevel=1")
+        conceallevel = 1
+      end
+    end,
+    desc = "Toggle UI"
   },
   { "<CR>", require("obsidian").util.smart_action, desc = "Smart action" },
 }
