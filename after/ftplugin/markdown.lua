87557463ac45fc78dafe8e54c171d1a472721914
RinZ <supawit.promma6256@gmail.com>
2025-07-07 16:29:02 +0700

    Add template support for Obsidian

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,12 +3,19 @@ vim.cmd("setlocal textwidth=80")
 vim.opt_local.spell = true
 vim.opt_local.conceallevel = 1
 
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.expandtab = true
+vim.opt_local.smartindent = true
+
 local which_key = require("which-key")
 
 local obsidian_mappings = {
-  { "<leader>c",  group = "Obsidian" },
-  { "<leader>ch", require("obsidian").util.toggle_checkbox, desc = "Toggle checkbox" },
-  { "<CR>",       require("obsidian").util.smart_action,    desc = "Smart action" },
+  { "<leader>o",  group = "Obsidian" },
+  { "<leader>on", "<CMD>ObsidianTemplate Note<CR>",      desc = "Insert note template" },
+  { "<leader>ot", "<CMD>ObsidianTemplate<CR>",           desc = "Select Template" },
+  { "<CR>",       require("obsidian").util.smart_action, desc = "Smart action" },
 }
 
 which_key.add(obsidian_mappings)
