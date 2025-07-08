3d55314b433c146721ee14c2181ad7143cde1fa1
RinZ <supawit.promma6256@gmail.com>
2025-07-09 00:39:27 +0700

    Edit c.lua

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,6 +1,5 @@
-vim.bo.tabstop = 4        -- How many spaces a <Tab> character takes up
-vim.bo.shiftwidth = 4     -- How many spaces are used for an indent level (for >>, ==, etc.)
-vim.bo.expandtab = true   -- Use spaces instead of tabs when pressing <Tab>
-vim.bo.autoindent = true  -- Copy indent from previous line
-vim.bo.smartindent = true -- Smarter C-style indentation
-vim.bo.cindent = true     -- Even smarter C-style indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.cindent = true -- Even smarter C-style indentation
