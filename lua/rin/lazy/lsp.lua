a1e4ad1a818ec1e82f214d9d5ebfd48ff1de9c34
RinZ <supawit.promma6256@gmail.com>
2025-07-10 01:16:22 +0700

    Rewrite lsp to use vim.lsp.config

--- lua/rin/lazy/lsp.lua
+++ lua/rin/lazy/lsp.lua
@@ -1,149 +1,144 @@
 -- yoink from https://github.com/VVoruganti/dotfiles/blob/master/neovim/lua/marshmalon/lazy/lsp.lua
 
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "j-hui/fidget.nvim",
-  },
-  config = function()
-    local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities()
-    )
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+    },
+    config = function()
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities()
+        )
 
-    require("fidget").setup({})
-    require("mason").setup()
+        require("fidget").setup({})
+        require("mason").setup()
 
-    require('mason-lspconfig').setup({
-      ensure_installed = {
-        "lua_ls",
-        "ts_ls"
-      },
-      handlers = {
-        function(server_name)
-          require('lspconfig')[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-        lua_ls = function()
-          require('lspconfig').lua_ls.setup({
+        require('mason-lspconfig').setup({
+            ensure_installed = {
+                "lua_ls",
+                "ts_ls"
+            },
+            automatic_enable = true,
+            handlers = ({
+                jdtls = function()
+                    require('java').setup({})
+                    require('lspconfig').jdtls.setup({
+                        capabilities = capabilities
+                    })
+                end
+            })
+        })
+
+        vim.lsp.config('*', {
+            capabilities = capabilities
+        })
+
+        vim.lsp.config('lua_ls', {
             capabilities = capabilities,
             settings = {
-              Lua = {
-                runtime = {
-                  version = 'LuaJIT'
-                },
-                diagnostics = {
-                  globals = { 'vim', 'love' },
-                },
-                workspace = {
-                  library = {
-                    vim.env.VIMRUNTIME,
-                  }
+                Lua = {
+                    diagnostics = {
+                        globals = { 'vim' }
+                    }
                 }
-              }
             }
-          })
-        end,
+        })
+
         -- https://www.reddit.com/r/neovim/comments/1g4e3sa/finally_neovim_native_vue_lsp_perfection_2024/
-        ts_ls = function()
-          require('lspconfig').ts_ls.setup({
-              capabilities = capabilities,
-              init_options = {
+        vim.lsp.config('ts_ls', {
+            capabilities = capabilities,
+            init_options = {
                 plugins = { -- I think this was my breakthrough that made it work
-                  {
-                    name = "@vue/typescript-plugin",
-                    location = vim.fn.stdpath 'data' ..
-                        '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-                    languages = { "vue" },
-                  },
+                    {
+                        name = "@vue/typescript-plugin",
+                        location = vim.fn.stdpath 'data' ..
+                            '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                        languages = { "vue" },
+                    },
                 },
-              },
-              filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
             },
+            filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        })
 
-            require('lspconfig').volar.setup({})
-          )
-        end,
-        jdtls = function()
-          require('java').setup({})
+        vim.lsp.config('jdtls', {
+            handlers = {
+                -- By assigning an empty function, you can remove the notifications
+                -- printed to the cmd
+                ["$/progress"] = function(_, result, ctx) end,
+            },
+        })
 
-          require('lspconfig').jdtls.setup({
-            capabilities = capabilities
-          })
-        end,
-      }
-    })
 
-    local cmp = require('cmp')
-    local luasnip = require("luasnip")
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local cmp = require('cmp')
+        local luasnip = require("luasnip")
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
+        local has_words_before = function()
+            unpack = unpack or table.unpack
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
 
-    -- this is the function that loads the extra snippets to luasnip
-    -- from rafamadriz/friendly-snippets
-    require('luasnip.loaders.from_vscode').lazy_load()
+        -- this is the function that loads the extra snippets to luasnip
+        -- from rafamadriz/friendly-snippets
+        require('luasnip.loaders.from_vscode').lazy_load()
 
-    cmp.setup({
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      sources = {
-        { name = 'path' },
-        { name = 'nvim_lsp' },
-        { name = 'luasnip', keyword_length = 2 },
-        { name = 'buffer',  keyword_length = 3 },
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- they way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }),
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-      },
-    })
-  end,
+        cmp.setup({
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            sources = {
+                { name = 'path' },
+                { name = 'nvim_lsp' },
+                { name = 'luasnip', keyword_length = 2 },
+                { name = 'buffer',  keyword_length = 3 },
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                        -- they way you will only jump inside the snippet region
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            }),
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+        })
+    end,
 }
